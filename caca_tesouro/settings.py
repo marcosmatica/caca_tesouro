d9c090e6066514b7f65f8d191e0ef560b08ea2bd
marcosmatica <marcosgt_rodrigues@hotmail.com>
2025-11-04 21:47:11 -0300

    att

--- caca_tesouro/settings.py
+++ caca_tesouro/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'sua-chave-secreta-aqui-mude-em-producao'
 DEBUG = True
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*', 'localhost', 'ifbrecanto.pythonanywhere.com']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
